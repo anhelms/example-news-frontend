cc42ed8d07ec66e2629d3e9edd880776468e5656
Anna N Helms <unicornbuddha@gmail.com>
2023-12-05 17:07:29 -0800

    adds Content, ArticlesIndex, and css

--- src/ArticlesIndex.jsx
+++ src/ArticlesIndex.jsx
@@ -1,7 +1,17 @@
-export function ArticlesIndex() {
+export function ArticlesIndex(props) {
+  console.log(props.articles);
     return (
       <div>
-        <h1>All Articles</h1>
+        <h1>All Lego Articles</h1>
+        {props.articles.map((article) => (
+          <div key={article.title}>
+            <h2>{article.title}</h2>
+            <img src={article.urlToImage} alt={article.title} />
+            <p>Author: {article.author}</p>
+            <p>Description: {article.description}</p>
+            <p>Source: {article.source.name}</p>
+            </div>
+        ))}
       </div>
     );
   }
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,24 @@
-import { ArticlesIndex } from "/.ArticlesIndex";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { ArticlesIndex } from "./ArticlesIndex";
 
 export function Content() {
+   const [articles, setArticles] = useState([]);
+
+   const handleArticlesIndex = () => {
+        axios
+            .get(`https://newsapi.org/v2/everything?q=lego&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`)
+            .then((response) => {
+                console.log(response.data.articles);
+                setArticles(response.data.articles);
+        });
+    };
+
+    useEffect(handleArticlesIndex,[]);
+   
     return (
       <div>
-       <ArticlesIndex />
+       <ArticlesIndex articles={articles}/>
       </div>
     )
   }
