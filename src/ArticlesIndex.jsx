5da50c167f72e7aaadb333d9ca9c7b8986662e77
Anna N Helms <unicornbuddha@gmail.com>
2023-12-06 15:54:44 -0800

    Adds minor tailwindcss style to ArticlesIndex

--- src/ArticlesIndex.jsx
+++ src/ArticlesIndex.jsx
@@ -2,11 +2,11 @@ export function ArticlesIndex(props) {
   console.log(props.articles);
     return (
       <div>
-        <h1 className="text-4xl font-bold underline text-red-400">All Lego Articles</h1>
+        <h1 className="text-4xl font-bold underline text-red-400 p-4 text-center">All Lego Articles</h1>
         {props.articles.map((article) => (
           <div key={article.title}>
-            <h2>{article.title}</h2>
-            <img src={article.urlToImage} alt={article.title} />
+            <h2 className="text-lg font-bold p-1">{article.title}</h2>
+            <img className="object-none object-center" src={article.urlToImage} alt={article.title} />
             <p>Author: {article.author}</p>
             <p>Description: {article.description}</p>
             <p>Source: {article.source.name}</p>
